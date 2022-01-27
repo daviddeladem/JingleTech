d05acfc7e4f9757bfb6404a6541992b47e9e7590
daviddeladem <33095955+daviddeladem@users.noreply.github.com>
2022-01-27 18:09:58 +0000

    added set products function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import Header from './containers/Header';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ProductListings from './containers/ProductListings';
-import ProductDetails from './containers/ProductDetails';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-       
-          <Router>
-            <Header />
-            <Switch>
-              <Route path="/" exact component= {ProductListings }/>
-              <Route path="/product/:productid" exact component= {ProductDetails}/>
-              <Route>404 Not Found</Route>
-            </Switch>
-          </Router>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
-};
+}
 
 export default App;
